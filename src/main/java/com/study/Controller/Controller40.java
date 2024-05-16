3034e10803d3e246add2e6f9c6a88e3116ba04a1
jung tae wook <xodnr901901@naver.com>
2024-05-16 10:17:27 +0900

    react 에서 get , post 받기2

--- src/main/java/com/study/Controller/Controller40.java
+++ src/main/java/com/study/Controller/Controller40.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.awt.*;
 
@@ -31,6 +32,16 @@ public class Controller40 {
     @PostMapping("/api/someurl2")
     public void method4() {
         System.out.println("끼얏호우");
+    }
+
+    @GetMapping("/api/someurl3")
+    public void method5(@RequestParam("id") Integer id,
+                        @RequestParam("name") String name,
+                        @RequestParam("email") String email) {
+
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
     }
 
 }
