efb79ddee5a4259a9e54fbf48a1ebcb011ac163d
jung tae wook <xodnr901901@naver.com>
2024-05-16 14:49:15 +0900

    자바빈 객체응답 json 으로 직렬화 할떄 get 메소드로 프로퍼티가 결정됨

---
+++ src/main/java/com/study/Controller/Controller42.java
@@ -0,0 +1,66 @@
+package com.study.Controller;
+
+import com.study.domain.MyBean411;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api/main42")
+public class Controller42 {
+
+    @GetMapping("sub1")
+    @ResponseBody
+    public String method1() {
+
+        return "어떤 데이터";
+    }
+
+    @GetMapping("sub2")
+    @ResponseBody
+    public String method2() {
+        double random = Math.random();
+        String result = "";
+        if (random < 1.0 / 3) {
+            return "가위";
+        } else if (random < 2.0 / 3) {
+            return "바위";
+        }
+        return "보";
+    }
+
+    @GetMapping("sub3")
+    @ResponseBody
+    public Map<String, Object> method3() {
+        return Map.of("name", "흥민",
+                "age", 35,
+                "married", true,
+                "info", Map.of("team", "토트넘",
+                        "location", "런던"),
+                "item", List.of("축구공", "운동화", "유니폼"));
+    }
+
+    @GetMapping("sub4")
+    @ResponseBody
+    public Map<String, Object> method4() {
+        return Map.of(
+                "name","호우",
+                "age",15,
+                "like","사향고양이",
+                "animal","고양이"
+        );
+    }
+    @GetMapping("sub5")
+    @ResponseBody
+    public MyBean411 method5() {
+        MyBean411 obj = new MyBean411();
+        obj.setName("동글");
+        obj.setAge(18);
+
+        return obj;
+    }
+}
